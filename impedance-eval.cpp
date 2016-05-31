e9616a37e1e55adf0dfb310a7a9f028d31d36176
Yogi Patel <yapatel@gatech.edu>
2016-05-31 08:12:56 -0400

    finalized module; need to add example workspace

--- impedance-eval.cpp
+++ impedance-eval.cpp
@@ -1,47 +1,41 @@
 /*
-	Copyright (C) 2011 Georgia Institute of Technology, University of Utah, Weill Cornell Medical College
+			Copyright (C) 2011 Georgia Institute of Technology, University of Utah, Weill Cornell Medical College
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+			This program is free software: you can redistribute it and/or modify
+			it under the terms of the GNU General Public License as published by
+			the Free Software Foundation, either version 3 of the License, or
+			(at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+			This program is distributed in the hope that it will be useful,
+			but WITHOUT ANY WARRANTY; without even the implied warranty of
+			MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+			GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/*
- * This is a template implementation file for a user module derived from
- * DefaultGUIModel with a custom GUI.
- */
+			You should have received a copy of the GNU General Public License
+			along with this program.  If not, see <http://www.gnu.org/licenses/>.
+			*/
 
 #include "impedance-eval.h"
 #include <main_window.h>
-#include <iostream>
 
 extern "C" Plugin::Object *createRTXIPlugin(void){
 	return new ImpedanceEval();
 }
 
 static DefaultGUIModel::variable_t vars[] = {
-	{ "GUI label", "Tooltip description", DefaultGUIModel::PARAMETER
-		| DefaultGUIModel::DOUBLE, },
-	{ "A State", "Tooltip description", DefaultGUIModel::STATE, }, 
+	{ "Source Signal", "Signal delivered to the electrode in volts", DefaultGUIModel::INPUT, },
+	{ "Measured Signal", "Signal measured after the electrode in volts", DefaultGUIModel::INPUT, },
+	{ "Duration", "Duration for measurement in seconds", DefaultGUIModel::PARAMETER, }, 
+	{ "Impedance", "Computed impedance in ohms", DefaultGUIModel::STATE, }, 
 };
 
 static size_t num_vars = sizeof(vars) / sizeof(DefaultGUIModel::variable_t);
 
-ImpedanceEval::ImpedanceEval(void) : DefaultGUIModel("ImpedanceEval with Custom GUI", ::vars, ::num_vars) {
-	setWhatsThis("<p><b>ImpedanceEval:</b><br>QWhatsThis description.</p>");
+ImpedanceEval::ImpedanceEval(void) : DefaultGUIModel("Impedance Evaluation", ::vars, ::num_vars) {
+	setWhatsThis("<p><b>Impedance Evaluation:</b><br>Measureme electrode impedance in RTXI.</p>");
 	DefaultGUIModel::createGUI(vars, num_vars); // this is required to create the GUI
 	customizeGUI();
-	initParameters();
-	update( INIT ); // this is optional, you may place initialization code directly into the constructor
+	update(INIT); // this is optional, you may place initialization code directly into the constructor
 	refresh(); // this is required to update the GUI with parameter and state values
 	QTimer::singleShot(0, this, SLOT(resizeMe()));
 }
@@ -49,36 +43,41 @@ ImpedanceEval::ImpedanceEval(void) : DefaultGUIModel("ImpedanceEval with Custom 
 ImpedanceEval::~ImpedanceEval(void) { }
 
 void ImpedanceEval::execute(void) {
+	source.push_back(input(0)); // V
+	measured.push_back(input(1)); // mA with 1kO r_sense
 	return;
-}
-
-void ImpedanceEval::initParameters(void) {
-	some_parameter = 0;
-	some_state = 0;
 }
 
 void ImpedanceEval::update(DefaultGUIModel::update_flags_t flag) {
 	switch (flag) {
 		case INIT:
-			period = RT::System::getInstance()->getPeriod() * 1e-6; // ms
-			setParameter("GUI label", some_parameter);
-			setState("A State", some_state);
+			period = RT::System::getInstance()->getPeriod() * 1e-9; // ns
+			duration = 5; // s
+			n = duration / period;
+			impedance = 0.0;
+			setState("Impedance", impedance);
+			setParameter("Duration", duration);
+			source.resize(n);
+			measured.resize(n);
+			reset();
 			break;
-	
+
 		case MODIFY:
-			some_parameter = getParameter("GUI label").toDouble();
+			duration = getParameter("Duration").toDouble();
 			break;
 
 		case UNPAUSE:
+			reset();
 			break;
 
 		case PAUSE:
+			compute();
 			break;
 
 		case PERIOD:
-			period = RT::System::getInstance()->getPeriod() * 1e-6; // ms
+			period = RT::System::getInstance()->getPeriod() * 1e-9; // ns
 			break;
-	
+
 		default:
 			break;
 	}
@@ -86,23 +85,48 @@ void ImpedanceEval::update(DefaultGUIModel::update_flags_t flag) {
 
 void ImpedanceEval::customizeGUI(void) {
 	QGridLayout *customlayout = DefaultGUIModel::getLayout();
-	
 	QGroupBox *button_group = new QGroupBox;
-	
-	QPushButton *abutton = new QPushButton("Button A");
-	QPushButton *bbutton = new QPushButton("Button B");
+	QPushButton *abutton = new QPushButton("Reset");
 	QHBoxLayout *button_layout = new QHBoxLayout;
 	button_group->setLayout(button_layout);
 	button_layout->addWidget(abutton);
-	button_layout->addWidget(bbutton);
-	QObject::connect(abutton, SIGNAL(clicked()), this, SLOT(aBttn_event()));
-	QObject::connect(bbutton, SIGNAL(clicked()), this, SLOT(bBttn_event()));
-	
+	QObject::connect(abutton, SIGNAL(clicked()), this, SLOT(reset()));
 	customlayout->addWidget(button_group, 0,0);
 	setLayout(customlayout);
 }
 
-// functions designated as Qt slots are implemented as regular C++ functions
-void ImpedanceEval::aBttn_event(void) { }
+void ImpedanceEval::reset(void)
+{
+	source.clear();
+	measured.clear();
+}
 
-void ImpedanceEval::bBttn_event(void) { }
+void ImpedanceEval::compute()
+{
+	double source_rms = computeMag(source);
+	double measured_rms = computeMag(measured);
+	impedance = source_rms / measured_rms;
+}
+
+double ImpedanceEval::computeMag(std::vector<double>& v)
+{
+	square(v);
+	double mean_val = mean(v);
+	double rms = std::sqrt(mean_val);
+	return rms;
+}
+
+void ImpedanceEval::square(std::vector<double>& v)
+{
+	std::transform(v.begin(), v.end(), v.begin(), [](double d) { return d * d; });
+}
+
+double ImpedanceEval::sum(const std::vector<double>& v)
+{
+	return std::accumulate(v.begin(), v.end(), 0.0, std::plus<double>());
+}
+
+double ImpedanceEval::mean(const std::vector<double>& v)
+{
+	return sum(v) / v.size();
+}
--- impedance-eval.h
+++ impedance-eval.h
@@ -1,53 +1,54 @@
 /*
-	Copyright (C) 2011 Georgia Institute of Technology, University of Utah, Weill Cornell Medical College
+			Copyright (C) 2011 Georgia Institute of Technology, University of Utah, Weill Cornell Medical College
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+			This program is free software: you can redistribute it and/or modify
+			it under the terms of the GNU General Public License as published by
+			the Free Software Foundation, either version 3 of the License, or
+			(at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+			This program is distributed in the hope that it will be useful,
+			but WITHOUT ANY WARRANTY; without even the implied warranty of
+			MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+			GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/*
-* This is a template header file for a user modules derived from
-* DefaultGUIModel with a custom GUI.
-*/
+			You should have received a copy of the GNU General Public License
+			along with this program.  If not, see <http://www.gnu.org/licenses/>.
+			*/
 
 #include <default_gui_model.h>
+#include <basicplot.h>
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
 
 class ImpedanceEval : public DefaultGUIModel {
-	
+
 	Q_OBJECT
-	
+
 	public:
 		ImpedanceEval(void);
 		virtual ~ImpedanceEval(void);
-	
+
 		void execute(void);
 		void createGUI(DefaultGUIModel::variable_t *, int);
 		void customizeGUI(void);
-	
+
 	protected:
 		virtual void update(DefaultGUIModel::update_flags_t);
-	
-	private:
-		double some_parameter;
-		double some_state;
-		double period;
 
-		void initParameters();
-	
-	private slots:
-		// these are custom functions that can also be connected to events 
-		// through the Qt API. they must be implemented in plugin_template.cpp
-	
-		void aBttn_event(void);
-		void bBttn_event(void);
+	private:
+		int n;
+		double impedance;
+		double period;
+		double duration;
+		std::vector<double> source; // buffer for data before event
+		std::vector<double> measured; // buffer for data before event
+		void compute();
+		double computeMag(std::vector<double>& v);
+		void square(std::vector<double>& v);
+		double mean(const std::vector<double>& v);
+		double sum(const std::vector<double>& v);
+
+		private slots:
+			void reset(void);
 };
